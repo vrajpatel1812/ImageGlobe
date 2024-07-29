65f4d11e85c47689ce7fbe8464efc49cb555abff
vrajpatel1812 <vrajp771@gmail.com>
2024-07-29 10:58:26 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import "./App.css";
+import { Container } from "@mui/material";
 import ImageList from "./component/imageList/ImageList";
 import Nav from "./component/Nav";
 import Upload from "./component/upload/Upload";
 
 function App() {
   return (
-    <>
+    <Container maxWidth="lg" sx={{ textAlign: "center", mt: "3rem" }}>
       <Nav />
       <Upload />
       <ImageList />
-    </>
+    </Container>
   );
 }
 
